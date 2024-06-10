18689535d14c74be53c7bd7a3a24f06ca5949dd2
Utkarsh Rathore <utkarshsinghrathore1@gmail.com>
2024-06-11 01:40:33 +0530

    fixed css in detail section

--- src/app/pokemon/[name]/page.tsx
+++ src/app/pokemon/[name]/page.tsx
@@ -22,45 +22,47 @@ const PokemonDetail = () => {
         {' > '}
         <span>{data.name}</span>
       </nav>
-      <div className="border rounded flex flex-col  bg-[#FCC666]">
-        <div className='bg-[#60E2C9] w-full flex justify-center items-center'>
-          <img src={data.sprites.front_default} alt={data.name} className='h-auto w-1/2'/>
-        </div>
-        <div className='p-4'>
-          <p><span className='font-bold'>Name: </span> {data.name.charAt(0).toLocaleUpperCase()+data.name.slice(1)}</p>
-          <p><span className='font-bold'>Type: </span> {data.types.map( (item: { type: { name: string; }; }, index: number) => {
-            if(index === data.types.length -1) {
-              return item.type.name
-            }
-            else {
-              return item.type.name+", "
-            }
-            
-            })}</p>
-          <p><span className='font-bold'>Base Experience: </span> {data.stats.map( (item: { stat: { name: string; }; }, index: number) => {
-            if(index === data.stats.length -1) {
-              return item.stat.name
-            }
-            else {
-              return item.stat.name+", "
-            }
-            })}</p>
-            <p><span className='font-bold'>Abilities: </span> {data.abilities.map( (item: { ability: { name: string; }; }, index: number) => {
-            if(index === data.abilities.length -1) {
-              return item.ability.name
-            }
-            else {
-              return item.ability.name+", "
-            }
-            })}</p>
-            <p><span className='font-bold'>Some Moves: </span> {data.moves.slice(0,7).map( (item: { move: { name: string; }; }, index: number) => {
-            if(index === 6) {
-              return item.move.name
-            }
-            else {
-              return item.move.name+", "
-            }
-            })}</p>
+      <div className='w-full flex items-center justify-center mt-32'>
+        <div className="border rounded flex flex-col w-1/3 bg-[#FCC666]">
+          <div className='bg-[#60E2C9] w-full flex justify-center items-center'>
+            <img src={data.sprites.front_default} alt={data.name} className='h-auto w-1/2'/>
+          </div>
+          <div className='p-4'>
+            <p><span className='font-bold'>Name: </span> {data.name.charAt(0).toLocaleUpperCase()+data.name.slice(1)}</p>
+            <p><span className='font-bold'>Type: </span> {data.types.map( (item: { type: { name: string; }; }, index: number) => {
+              if(index === data.types.length -1) {
+                return item.type.name
+              }
+              else {
+                return item.type.name+", "
+              }
+              
+              })}</p>
+            <p><span className='font-bold'>Base Experience: </span> {data.stats.map( (item: { stat: { name: string; }; }, index: number) => {
+              if(index === data.stats.length -1) {
+                return item.stat.name
+              }
+              else {
+                return item.stat.name+", "
+              }
+              })}</p>
+              <p><span className='font-bold'>Abilities: </span> {data.abilities.map( (item: { ability: { name: string; }; }, index: number) => {
+              if(index === data.abilities.length -1) {
+                return item.ability.name
+              }
+              else {
+                return item.ability.name+", "
+              }
+              })}</p>
+              <p><span className='font-bold'>Some Moves: </span> {data.moves.slice(0,7).map( (item: { move: { name: string; }; }, index: number) => {
+              if(index === 6) {
+                return item.move.name
+              }
+              else {
+                return item.move.name+", "
+              }
+              })}</p>
+          </div>
         </div>
       </div>
     </div>
